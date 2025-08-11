39741880da7456c57cbe84b04fa189d845043eda
Vladyslav Suhovetski <vlad.suhovetsky@gmail.com>
2025-08-11 09:22:41 +0200

    Test commit

--- pages/home.py
+++ pages/home.py
@@ -4,7 +4,6 @@ from utils import helper as u
 from sqlalchemy.orm import sessionmaker
 from db.models import MtProcessingState
 from db.generic_utils import get_unique_column_values, get_paginated_data, get_total_count_orm, home_query
-from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.title("Przetwarzania DMSF")
 
@@ -118,41 +117,7 @@ if not st.session_state.home_initial_load_done or filters_changed:
 
 # Wyświetlenie danych
 if st.session_state.home_data_cache is not None and not st.session_state.home_data_cache.empty and len(st.session_state.home_data_cache.columns) > 0:
-    gb = GridOptionsBuilder.from_dataframe(st.session_state.home_data_cache)
-    gb.configure_default_column(
-        filter=True,
-        sortable=True,
-        groupable=True,
-        resizable=True,
-        wrapText=True,
-        autoHeight=True,
-        enableRowGroup=True,
-        enablePivot=True,
-        enableValue=True,
-        headerCheckboxSelection=True,
-    )
-    gb.configure_grid_options(
-        pagination=True,
-        paginationAutoPageSize=False,
-        domLayout='normal',
-        suppressRowClickSelection=False,
-        rowSelection='single',
-        enableRangeSelection=True,
-        suppressAggFuncInHeader=False,
-        animateRows=True,
-        defaultColDef={
-            "resizable": True,
-            "sortable": True,
-            "filter": True
-        },
-        groupSelectsChildren=True,
-        suppressRowDeselection=False,
-        suppressCellSelection=False,
-        suppressHorizontalScroll=False
-    )
-    gb.configure_side_bar()
-    grid_options = gb.build()
-    AgGrid(st.session_state.home_data_cache, gridOptions=grid_options, enable_enterprise_modules=True, theme="streamlit", update_mode="NO_UPDATE", pagination=True, height=400)
+    st.dataframe(st.session_state.home_data_cache, use_container_width=True)
     st.markdown(f"**Showing {len(st.session_state.home_data_cache)} of {st.session_state.home_total_count} records**")
 else:
     st.info("No data to display for the selected filters.")
